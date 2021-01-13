29b959d4abb2e1f3b4618976070093d06e1ea4d4
asanz060 <asanz060@ikasle.ehu.eus>
2021-01-14 00:20:34 +0800

    fuentes corregidas

--- Caso3/node.cc
+++ Caso3/node.cc
@@ -7,7 +7,18 @@
 using namespace omnetpp;
 
 class node : public cSimpleModule{
+    protected:
+        virtual void initialize() override;
+        virtual void handleMessage(cMessage *msg) override;
 
 };
 
 Define_Module(node);
+
+void node::initialize(){
+
+}
+
+void node::handleMessage(cMessage *msg){
+    delete msg;
+}
--- Caso3/source.cc
+++ Caso3/source.cc
@@ -13,60 +13,48 @@ class source : public cSimpleModule{
         int n=100;
         int seq=0;
         simtime_t startTime=0;
+        cMessage *event;
     protected:
         virtual void initialize() override;
         virtual void handleMessage(cMessage *msg) override;
-        virtual double getPacketLenght(double lambda);
-        virtual double getTime(double meanLength);
         virtual myPacket* generarPaquete();
 };
 
 Define_Module(source);
 
 void source::initialize(){
-    for(int i=0;i<n;i++){
-        double l=getPacketLenght(meanLength);
-        double t=getTime(lambda);
-        myPacket* paquete=generarPaquete();
-        paquete->setBitLength(l);
-        scheduleAt(simTime()+t, paquete);
-    }
+    event=new cMessage("event");
+
+    double t=exponential(0.5, 0);
+    EV << "\nTime: " << t;
+    scheduleAt(simTime()+t, event);
 }
 
 void source::handleMessage(cMessage *msg){
-    myPacket* p=check_and_cast<myPacket*>(msg);
-    send(p,"out");
-}
+    EV << "\nSending packet from Source " << getIndex() << " At time " << simTime();
+    myPacket* paquete=generarPaquete();
 
-double source::getPacketLenght(double meanLength){
-    std::exponential_distribution<double> randomExp(1/meanLength);
-    std::default_random_engine generator;
+    cMessage *prueba=new cMessage("prueba");
+    send(paquete,"out");
+    double t=exponential(0.5, 0);
+    scheduleAt(simTime()+t, event);
 
-    double l=randomExp(generator);
-    //printf("%f ",l);
-    return l;
-}
-double source::getTime(double lambda){
-    std::exponential_distribution<double> randomExp(1/lambda);
-    std::default_random_engine generator;
+    //myPacket* p=check_and_cast<myPacket*>(msg);
 
-    double t=randomExp(generator);
-    //printf("%f ",t);
-    return t;
 }
 
 myPacket* source::generarPaquete(){
-    //std::string name=getName()+"Packet"+std::to_string(seq);
     char packetName[20];
-    strcpy(packetName,getName());
-    strcpy(packetName, "Packet");
-    std::string id=std::to_string(seq);
-    strcpy(packetName, id.c_str());
+    sprintf(packetName,"Source%dPacket%d",getIndex(),seq);
+
     myPacket* p=new myPacket(packetName);
     p->setSeq(seq);
     p->setSource(getId());
     p->setType(1);
     seq++;
+    double l=exponential(1000, 0);
+    EV << "\nPacket length: " << l << " Seq: " << seq;
+    p->setBitLength(l);
 
     return p;
 }
